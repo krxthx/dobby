622cc49264fbc30ffcdcfb28fc0240b93cd94d4d
krithika117 <krithikanithyanandam@gmail.com>
2022-02-06 20:10:47 +0530

    speech recognize

--- meenakshi.py
+++ meenakshi.py
@@ -24,13 +24,23 @@ def greeting():
 
 
 def takeCommand():
-    # It takes microphone input from user and gives a string o/p
+    ''' It takes microphone i/p from user and gives a string o/p '''
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print('Listening.')
         r.pause_threshold = 1
         audio = r.listen(source)
+    try:
+        print("Processing.")
+        query = r.recognize_google(audio, language="en-in")
+        print("You said: " + query + "\n")
+    except Exception as e:
+        # print(e)
+        speak("I'm sorry, could you please repeat that?")
+        return "None"
+    return query
 
 
 if __name__ == "__main__":
     greeting()
+    takeCommand()
