56d69b1fba835ba92167eb44088d4df007b215d9
krithika117 <krithikanithyanandam@gmail.com>
2022-02-07 11:32:28 +0530

    bye note

--- meenakshi.py
+++ meenakshi.py
@@ -29,61 +29,70 @@ def takeCommand():
     ''' It takes microphone i/p from user and gives a string o/p '''
     r = sr.Recognizer()
     with sr.Microphone() as source:
+        print("Clearing background noises...Please wait")
+        r.adjust_for_ambient_noise(source, duration=0.5)
         print('Listening.')
-        r.pause_threshold = 1
-        audio = r.listen(source)
-
+        recordedaudio = r.listen(source)
     try:
-        print("Processing.")
-        query = r.recognize_google(audio, language="en-in")
-        print("You said: " + query + "\n")
+        query = r.recognize_google(recordedaudio, language='en-in')
+        if 'meenakshi' in query:
+            query = query.replace('Meenakshi', '')
+        query = query.lower()
+        print('Your message:', format(query))
 
     except Exception as e:
-        # print(e)
-        speak("I'm sorry, could you please repeat that?")
+        # speak("I'm sorry, could you please repeat that?")
         return "None"
+
+    # Functionalities.
+    # Wiki Searches.
+    if 'wikipedia' in query:
+        speak('Searching Wikipedia')
+        query = query.replace('wikipedia', '')
+        results = wiki.summary(query, sentences=2)
+        speak("According to wikipedia,")
+        speak(results)
+
+    # Definitions.
+    elif 'what is' in query:
+        speak('Searching Wikipedia')
+        query = query.replace('what is', '')
+        print(query)
+        results = wiki.summary(query, sentences=2)
+        speak("According to wikipedia,")
+        speak(results)
+
+    # Open YouTube.
+    elif 'open youtube' in query:
+        speak("Okay. YouTube opening now.")
+        webbrowser.open('youtube.com')
+        # break
+
+    # Open StackOverflow.
+    elif 'open stack overflow' in query:
+        speak("Okay. StackOverflow opening now.")
+        webbrowser.open('stackoverflow.com')
+        # break
+
+    # Open StackOverflow.
+    elif 'open google' in query:
+        speak("Okay. Google opening now.")
+        webbrowser.open('google.com')
+
+    # Bye Note.
+    elif 'bye' or 'good bye' or 'goodbye' or 'tata' in query:
+        speak('Call me if you need anything. Have a nice day.')
+        speak('Bye!')
+        exit()
+
+    else:
+        speak("I'm sorry, could you please repeat that?")
+
     return query
 
 
 if __name__ == "__main__":
     greeting()
     while True:
-        # Logic to execute tasks based on query.
-        query = takeCommand().lower()
-
-        # Functionalities.
-
-        # Wiki Searches.
-        if 'wikipedia' in query:
-            speak('Searching Wikipedia')
-            query = query.replace('wikipedia', '')
-            results = wiki.summary(query, sentences=2)
-            speak("According to wikipedia,")
-            speak(results)
-
-        # Definitions.
-        elif 'what is' in query:
-            speak('Searching Wikipedia')
-            query = query.replace('what is', '')
-            print(query)
-            results = wiki.summary(query, sentences=2)
-            speak("According to wikipedia,")
-            speak(results)
-
-        # Open YouTube.
-        elif 'open youtube' in query:
-            speak("Okay. YouTube opening now.")
-            webbrowser.open('youtube.com')
-            # break
-
-        # Open StackOverflow.
-        elif 'open stack overflow' in query:
-            speak("Okay. StackOverflow opening now.")
-            webbrowser.open('stackoverflow.com')
-            # break
-
-        # Bye Note.
-        # elif 'bye' or 'good bye' or 'goodbye' or 'tata' in query:
-        #     speak('Call me if you need anything. Have a nice day.')
-        #     speak('Bye!')
-        #     exit()
+        # query = takequery().lower()
+        takeCommand()
